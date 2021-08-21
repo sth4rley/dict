992cf2c0ee1f75dea115c5feb31241cc1c5929d3
sth4rley <sth4rley@gmail.com>
2021-08-21 02:06:00 -0300

    some changes

--- temp.js
+++ temp.js
@@ -2,18 +2,20 @@ const dictionary = require("./dictionaries/words.json");
 
 // SETUP
 
-const getRandomInt = (max) => Math.floor(Math.random() * max);
-const shuffleArray = (array) =>
+const randomInt = (max) => Math.floor(Math.random() * max);
+const merge = (...arrays) => arrays.reduce((acc, cur) => acc.concat(cur), []);
+const shuffle = (array) =>
   array
     .map((value) => ({ value, sort: Math.random() }))
     .sort((a, b) => a.sort - b.sort)
     .map(({ value }) => value);
 
+
 // funcao que gera lista de números
-const randomQuestionNumbers = (numberOfQuestions, arr) => {
-  let currentNumber = getRandomInt(dictionary.length);
-  if (!arr.includes(currentNumber)) arr.push(currentNumber); // se o número gerado não estiver na lista adicione-o
-  if (arr.length < numberOfQuestions) randomQuestionNumbers(numberOfQuestions, arr);
+const randomNumbers = (numberOfQuestions, arr = new Array()) => {
+  let currentNumber = randomInt(dictionary.length);
+  if (!arr.includes(currentNumber)) arr.push(currentNumber);
+  if (arr.length < numberOfQuestions) randomNumbers(numberOfQuestions, arr);
   if (arr.length == numberOfQuestions) return arr;
 };
 
@@ -21,3 +23,10 @@ const randomQuestionNumbers = (numberOfQuestions, arr) => {
 const numsToWords = (...numbersList) => numbersList.map((num) => dictionary[num].word);
 const nummsToTranslations = (...numbersList) => numbersList.map((num) => dictionary[num].translation);
 const numsToPronounces = (...numbersList) => numbersList.map((num) => dictionary[num].pronounce);
+
+const questionNumbers = randomNumbers(2)
+const words = numsToWords(...questionNumbers);
+const translations = nummsToTranslations(...questionNumbers);
+const alternatives = shuffle(merge(words, translations))
+
+console.log(alternatives);
