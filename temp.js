c6380307187edc70a3f0007eed2da0c8d3deda3f
sth4rley <sth4rley@gmail.com>
2021-08-20 21:22:28 -0300

    removed singular num function

--- temp.js
+++ temp.js
@@ -2,31 +2,22 @@ const dictionary = require("./dictionaries/words.json");
 
 // SETUP
 
-
-const getRandomInt = (max) => Math.floor(Math.random() * max)
-const shuffleArray = (array) => array
+const getRandomInt = (max) => Math.floor(Math.random() * max);
+const shuffleArray = (array) =>
+  array
     .map((value) => ({ value, sort: Math.random() }))
     .sort((a, b) => a.sort - b.sort)
-    .map(({ value }) => value)
+    .map(({ value }) => value);
 
 // funcao que gera lista de números
 const randomQuestionNumbers = (numberOfQuestions, arr) => {
-    if (arr.length < numberOfQuestions) { // adiciona números até completar o array
-        let currentNumber = getRandomInt(dictionary.length)
-        if (!arr.includes(currentNumber)) arr.push(currentNumber); // se não tiver o número adicione-o
-        randomQuestionNumbers(numberOfQuestions, arr)
-    }
-    if (arr.length == numberOfQuestions) return arr
-}
-
-// num -> word
-const getWord = (num) => dictionary[num].word
-const getTranslation = (num) => dictionary[num].translation
-const getPronounce = (num) => dictionary[num].pronounce
+  let currentNumber = getRandomInt(dictionary.length);
+  if (!arr.includes(currentNumber)) arr.push(currentNumber); // se o número gerado não estiver na lista adicione-o
+  if (arr.length < numberOfQuestions) randomQuestionNumbers(numberOfQuestions, arr);
+  if (arr.length == numberOfQuestions) return arr;
+};
 
 // num[] -> words[]
-const numsToWords = (numbersList) => numbersList.map(getWord)
-const nummsToTranslation = (numbersList) => numbersList.map(getTranslation)
-const NumsToPronounce = (numbersList) => numbersList.map(getPronounce)
-
-console.log(numsToWords([34,40,900]))
+const numsToWords = (...numbersList) => numbersList.map((num) => dictionary[num].word);
+const nummsToTranslations = (...numbersList) => numbersList.map((num) => dictionary[num].translation);
+const numsToPronounces = (...numbersList) => numbersList.map((num) => dictionary[num].pronounce);
